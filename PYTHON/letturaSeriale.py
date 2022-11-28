7000aec1995b3f58e4991498a82b33d7a11ec10b
Virginiaciveriati <virginia.civeriati@mail.polimi.it>
2022-11-28 14:57:36 +0100

    codice di lettura con menu per la scelta della porta seriale

--- PYTHON/letturaSeriale.py
+++ PYTHON/letturaSeriale.py
@@ -1,54 +1,43 @@
 import serial
 
-
+'''
 from enum import Enum
-
-
 class Gesture(Enum):
-    POLLICE = [255, 255, 0, 0]
-    APERTA = [0, 0, 0, 0]
-
+    POLLICE = [255,255,0,0]
+    APERTA = [0,0,0,0]
 
 class Packet():
-
+    
     lista = list()
 
-    def __init__(self, pacchetto: bytes) -> None:
-        self.lista = list(pacchetto)
+    def __init__(self, pacchetto : bytes) -> None:
+        self.valore = list(pacchetto)
 
     def getGesture(self) -> Gesture:
         return Gesture(self.lista[1:5])
-
-           
-
-
+                
+'''
 def main():
     # initialization
     ser = serial.Serial()
     ser.baudrate = 9600
     ser.port = 'COM7'
-    while (not ser.is_open):
+    while(not ser.is_open):
         try:
             ser.open()
         except:
             print("Didn't connect, trying again")
-    while (ser.is_open):
-      dato = ser.read(6)
-    print(dato, type(dato))
-
-
-    packet = Packet(dato)
+    while(ser.is_open):
+        dato = ser.read(10)
+        print(dato, type(dato))
+    #packet = Packet(dato)
     #lista = packet.getList()
-    # print(lista)
-    # if packet.isOpponibile():
+    #print(lista)
+    #if packet.isOpponibile():
     #    print("callback to w")
-    if (packet[0]==160 and packet[5]== 192):
-        print(packet.getGesture())
-    else :
-        print("error!")
-
     #print(packet.getGesture())
-
+        
 
 if __name__ == "__main__":
     main()
+    
