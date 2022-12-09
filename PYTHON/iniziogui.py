ad72a7d1a7d21b81a9532cfecb0c10b299a6fd0b
Virginiaciveriati <virginia.civeriati@mail.polimi.it>
2022-12-09 12:40:14 +0100

    aggiunta di altra finestra, pulsanti quadrati

--- PYTHON/iniziogui.py
+++ PYTHON/iniziogui.py
@@ -45,7 +45,7 @@ class SerialWorker(QRunnable):
         super().__init__()
         # init port, params and signals
         self.port = serial.Serial()
-        self.port_name = 'COM8'
+        self.port_name = 'COM3'
         self.baudrate = 9600 # hard coded but can be a global variable, or an input param
         self.signals = SerialWorkerSignals()
 
@@ -108,9 +108,9 @@ class MainWindow(QMainWindow):
         
         # define worker
         self.serial_worker = SerialWorker(None)
-
+        self.port_name = "COM3"
         super(MainWindow, self).__init__()
-
+        self.w = None
         # title and geometry
         self.setWindowTitle("Progetto 3")
         width = 600
@@ -130,6 +130,9 @@ class MainWindow(QMainWindow):
         # layout
         button_hlay = QHBoxLayout()
         button_hlay.addWidget(self.conn_btn)
+        button_hlay.addWidget(self.win_btn)
+        self.conn_btn.setFixedSize(200, 200)
+        self.win_btn.setFixedSize(200, 200)
         vlay = QVBoxLayout()
         vlay.addLayout(button_hlay)
       # vlay.addLayout(led_hlay)
@@ -140,31 +143,47 @@ class MainWindow(QMainWindow):
 
     def createButton(self):
 
-        self.conn_btn=QPushButton(
-            text="Start"
+        self.conn_btn = QPushButton(
+            text = "Start",
+            checkable = True
+        )
+
+        self.win_btn = QPushButton(
+            text = "Open New Window"
         )
 
         self.conn_btn.clicked.connect(self.on_click)
+        self.win_btn.clicked.connect(self.show_new_window)
+
 
     def on_click(self,checked):
-        #if checked:
+        if checked:
            # self.serial_worker.signals.status.connect(self.check_serialport_status)
             self.serial_worker.signals.device_port.connect(self.connected_device)
+            self.serial_worker.signals.status.connect(self.check_serialport_status)
             # execute the worker
             self.threadpool.start(self.serial_worker)
-        #else:
-             # kill thread
-           # self.serial_worker.is_killed = True
-           # self.serial_worker.killed()
-
-    '''
+        else:
+            #kill thread
+            self.serial_worker.is_killed = True
+            self.serial_worker.killed()
+            self.conn_btn.setText(
+                "Connect to port {}".format(self.port_name)
+            )
+            
+    
+    
     def check_serialport_status(self, port_name, status):
-        if status == 1:
+        if status == 0:
             self.conn_btn.setChecked(False)
-        elif status == 0:
+        elif status == 1:
+            # enable all the widgets on the interface
+            self.conn_btn.setText(
+                "Disconnect from port {}".format(port_name)
+            )
             logging.info("Connected to port {}".format(port_name))
-    '''
-   
+    
+
     def connected_device(self, port_name):
         """!
         @brief Checks on the termination of the serial worker.
@@ -178,6 +197,24 @@ class MainWindow(QMainWindow):
         """
         self.serial_worker.is_killed = True
         self.serial_worker.killed()
+    
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+        self.w.show()
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Altra Finestra")
+        width = 600
+        height = 420
+        self.setMinimumSize(width, height)
+
 
 
 if __name__ == '__main__':
